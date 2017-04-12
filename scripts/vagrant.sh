49e70cfbf4f6f3d08c36ce106f6dd03a3da513f3
Miguel Angel Falcón Muñoz <mangel.falcon.ext@juntadeandalucia.es>
2017-04-12 15:29:57 +0200

    cambios varios

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -3,6 +3,9 @@ set -x
 
 echo "Adding the vagrant ssh public key..."
 
+export http_proxy="http://proxy.cfv.junta-andalucia.es:3128"
+export https_proxy="http://proxy.cfv.junta-andalucia.es:3128"
+
 mkdir /home/vagrant/.ssh
 curl -Lo /home/vagrant/.ssh/authorized_keys https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub
 chown -R vagrant /home/vagrant/.ssh
