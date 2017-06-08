bb888434e773b1add426c935b166fd38b7744919
Miguel Angel Falcón Muñoz <mangel.falcon.ext@juntadeandalucia.es>
2017-06-08 16:05:38 +0200

    Se modifica para que se cree la imagen con particionado , en espaÃol

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -6,7 +6,9 @@ echo "Adding the vagrant ssh public key..."
 export http_proxy="http://proxy.cfv.junta-andalucia.es:3128"
 export https_proxy="http://proxy.cfv.junta-andalucia.es:3128"
 
-mkdir /home/vagrant/.ssh
+mkdir /home/vagrant/.ssh/
 curl -Lo /home/vagrant/.ssh/authorized_keys https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub
 chown -R vagrant /home/vagrant/.ssh
 chmod -R go-rwsx /home/vagrant/.ssh
+
+sleep 100
